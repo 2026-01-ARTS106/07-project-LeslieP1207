a95cd1e205d308f55c1886f0dd9acc1f924899d2
LeslieP1207 <G00327175@stu.raritanval.edu>
2026-05-12 00:44:16 -0400

    main menu works

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void PlayGme()
+	{
+		SceneManager.LoadSceneAsync(1);
+	}
+	
 }
